937bc1a127a9f029df058a04460c6a09e94cd878
Marek <marekjan23@gmail.com>
2020-05-05 20:33:51 +0200

    Defined 1toN relationship with tickets

--- app/User.php
+++ app/User.php
@@ -36,4 +36,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // Relationships
+    public function tickets()
+    {
+        return $this->hasMany('App\Ticket');
+    }
 }
