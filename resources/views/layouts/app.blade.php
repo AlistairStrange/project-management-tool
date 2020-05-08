19a8a3279f5ecda229346af04c91229445e9d957
Marek <marekjan23@gmail.com>
2020-05-08 19:42:00 +0200

    Adding Tailwing CSS / JS dependencies

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
