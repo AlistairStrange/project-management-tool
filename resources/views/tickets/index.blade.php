5e72a7ef6855456cb1e81bd8e3775d343831ec42
Marek <marekjan23@gmail.com>
2020-05-10 16:00:17 +0200

    minor changes

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -7,22 +7,9 @@
             <div class="container mx-auto border-r border-gray-400 h-screen">
                 <h2 class="text-gray-600 text-center font-semibold">Open</h2>
 
-                <div class="container mx-auto">
-                    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-                        <div class="px-6 py-4">
-                            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-                            <p class="text-gray-700 text-base">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                            </p>
-                        </div>
-                        
-                        <div class="px-6 py-4">
-                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
-                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
-                        </div>
-                    </div>
-                </div>
+                @foreach($openTickets as $ticket)
+
+                @endforeach
 
             </div>
 
