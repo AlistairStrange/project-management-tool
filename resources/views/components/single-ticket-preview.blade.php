352a2bb58d28931ee9861f40b4acac8106699838
Marek <marekjan23@gmail.com>
2020-05-12 22:39:57 +0200

    Adjustment in layout, added priority symbol

--- resources/views/components/single-ticket-preview.blade.php
+++ resources/views/components/single-ticket-preview.blade.php
@@ -1,38 +1,72 @@
 <div class="container mx-auto">
     <div class="max-w-sm rounded overflow-hidden shadow-lg bg-gray-200 text-gray-700 mx-4 mt-3 float-left">
-        <div class="px-6 py-4">
+    
+        <div class="float-left py-2 pl-2">
+            @switch($ticket->priority)
+                @case('minimal')
+                    <!-- Minimal -->
+                    <p class="float-left pr-1 text-md font-semibold text-green-500 mx-auto">⇿</p>
+                    @break
+                @case('minor')
+                    <!-- Minor -->
+                    <p class="float-left pr-1 text-xs text-green-500 mx-auto">▲</p>
+                    @break
+                @case('major')
+                    <!-- Major -->
+                    <p class="float-left pr-1 text-xs text-red-500 mx-auto">▲</p>
+                    @break
+                @case('urgent')
+                    <!-- Urgent -->
+                    <p class="float-left pr-1 text-xs mx-auto">❗</p>
+                    @break
+                @case('blocker')
+                    <!-- Blocker -->
+                    <p class="float-left pr-1 text-xs text-red-600 font-bold mx-auto">Ø</p>
+                    @break
+            @endswitch
+        </div>
+
+        <div class="py-2 float-right">
             <a href="{{ route('ticket.show', $ticket->id) }}">
-                <h2 class="font-thin">{{$ticket->subject}}</h2>
+                <p class="font-thin text-sm">{{substr($ticket->subject, 0, 25) . '...'}}</p>
             </a>
         </div>
 
-        <!-- PLACEHOLDER show priority &  -->
+        <div class="container px-2 pt-4 pb-2 grid grid-cols-2">
+            <div class="col-span-1">
+                <p class="text-xs text-gray-600 bg-gray-300 rounded-md text-center float-left py-1 px-1">
+                    ID: ticket-{{ $ticket->id }}
+                </p>
+            </div>
 
-        <div class="px-6 py-4">
-            <p class="inline-block text-xs text-gray-600"><span class="font-semibold">Due: </span>{{ $ticket->deadline }}</p>
-            <p class="inline-block text-xs text-gray-600 items-center bg-gray-300 rounded-md px-2 py-1 float-right">
-                ID: ticket-{{ $ticket->id }}
-            </p>
+            <div class="col-span-1">
+                <p class="text-xs text-gray-600 float-right px-1 py-1"><span class="font-semibold">Due: </span>{{ $ticket->deadline }}</p>
+            </div>
         </div>
 
-        <!-- Moving ticket to adjacent status -->
-        @if (isset($ticket->status) && $ticket->status === 'Open') 
-            <a href="#" >
-                <p class="bg-gray-400 text-lg text-center text-white px-6">🠪</p>
-            </a>
-        @elseif (isset($ticket->status) && $ticket->status === 'Closed')
-            <a href="#" >
-                <p class="bg-gray-400 text-lg text-center text-white px-6">⟲</p>
-            </a>
-        @else
-            <a href="#" >
-                <p class="bg-gray-400 text-lg text-center text-white w-1/2 float-left border-r border-white">🠨</p>
-            </a>
 
-            <a href="#">
-                <p class="bg-gray-400 text-lg text-center text-white w-1/2 float-right">🠪</p>
-            </a>
-        @endif
+
+        <div class="container">
+            <!-- TICKET NAVIGATION -->
+            <!-- Moving ticket to adjacent status -->
+            @if (isset($ticket->status) && $ticket->status === 'Open') 
+                <a href="#" >
+                    <p class="bg-gray-400 text-lg text-center text-white px-6">🠪</p>
+                </a>
+            @elseif (isset($ticket->status) && $ticket->status === 'Closed')
+                <a href="#" >
+                    <p class="bg-gray-400 text-lg text-center text-white px-6">⟲</p>
+                </a>
+            @else
+                <a href="#" >
+                    <p class="bg-gray-400 text-lg text-center text-white w-1/2 float-left border-r border-white">🠨</p>
+                </a>
+    
+                <a href="#">
+                    <p class="bg-gray-400 text-lg text-center text-white w-1/2 float-right">🠪</p>
+                </a>
+            @endif
+        </div>
         
     </div>
 </div>
