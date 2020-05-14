12d45fdef2fe85998fb261579f1e25d37ae497a9
Marek <marekjan23@gmail.com>
2020-05-14 21:46:47 +0200

    Now showing also warning messages

--- resources/views/partials/_status.blade.php
+++ resources/views/partials/_status.blade.php
@@ -1,7 +1,13 @@
-<div class="flex bg-green-300 -mt-5">
-    @if(session('status'))
-        <div class="flex-auto text-center py-4 text-green-700 text-lg font-semibold">
+@if(session('status'))
+    <div class="flex bg-green-300 -mt-5">
+        <div class="flex-auto text-center py-4 text-green-700 text-md font-semibold">
             {{ session('status') }}
         </div>
-    @endif
-</div>
+    </div>
+@elseif(session('error'))
+    <div class="flex bg-red-300 -mt-5">
+        <div class="flex-auto text-center py-4 text-red-700 text-md font-semibold">
+            {{ session('error') }}
+        </div>
+    </div>
+@endif
