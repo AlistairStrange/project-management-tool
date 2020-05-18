ad5d6cddac8a8831ec48c36e5f0204cb0d385a9f
Marek <marekjan23@gmail.com>
2020-05-18 21:54:40 +0200

    Buttons for showing either all tickets or only assigned tickets added

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -1,7 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container my-6">
+        <a href="{{ route('tickets') }}" class="
+        {{ isset($btnAll) && $btnAll === 1 ? "bg-teal-400 text-white hover:bg-white hover:text-teal-500" : 'text-teal-500 hover:bg-teal-400 hover:text-white'}}
+        border border-teal-400 rounded text-sm ml-10 px-1 py-1">
+            All tickets   
+        </a>
+        
+        <a href="{{ route('my-tickets') }}" class="
+        {{ isset($btnAll) & $btnAll === 0 ? "bg-teal-400 text-white hover:bg-white hover:text-teal-500" : "hover:bg-teal-400 hover:text-white text-teal-500" }}
+        border border-teal-400 rounded text-sm  ml-5 px-1 py-1">
+            Only My tickets   
+        </a>
+    </div>
+
     <div class="container mx-auto">
+
         <div class="grid grid-cols-5 flex items-center">
             <!-- Open Tickets column -->
             <div class="container mx-auto border-r border-gray-400 h-screen">
