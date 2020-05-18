bccc36594d071e94fdab0083d8926f4c5fb66045
Marek <marekjan23@gmail.com>
2020-05-18 22:08:55 +0200

    Usear search jquery script moved to separate partials file -> included in create / edit views

--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -10,38 +10,8 @@
             </div>
        </div>
    </div>
-   
-       <!-- Script -->
-       <script type="application/javascript">
 
-        // CSRF Token
-        var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
-        $(document).ready(function(){
-    
-          $( "#user_search" ).autocomplete({
-            source: function( request, response ) {
-              // Fetch data
-              $.ajax({
-                url:"{{route('search-users')}}",
-                type: 'post',
-                dataType: "json",
-                data: {
-                   _token: CSRF_TOKEN,
-                   search: request.term
-                },
-                success: function( data ) {
-                   response( data );
-                }
-              });
-            },
-            select: function (event, ui) {
-               // Set selection
-               $('#user_search').val(ui.item.email); // display the selected text
-               $('#userid').val(ui.item.value); // save selected id to input
-               return false;
-            }
-          });
-    
-        });
-    </script>
+   <!-- JQUERY script for search & filter users -->
+   @include('partials._usersearch')
+
 @endsection
--- resources/views/tickets/edit.blade.php
+++ resources/views/tickets/edit.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="grid grid-flow-row">
-       <div class="container mx-auto">
-            <div class="max-w-lg rounded overflow-hidden shadow-lg mx-auto">
-                <div class="px-6 py-4">
-                    <x-ticket-form :ticket="$ticket"/>
+    <div class="grid grid-flow-row">
+        <div class="container mx-auto">
+                <div class="max-w-lg rounded overflow-hidden shadow-lg mx-auto">
+                    <div class="px-6 py-4">
+                        <x-ticket-form :ticket="$ticket"/>
+                    </div>
                 </div>
-            </div>
-       </div>
-</div>
+        </div>
+    </div>
+
+   <!-- JQUERY script for search & filter users -->
+   @include('partials._usersearch')
+
 @endsection
