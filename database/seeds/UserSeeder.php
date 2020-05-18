011f582c5ff956f3cc87fef3412efc5c709dd408
Marek <marekjan23@gmail.com>
2020-05-18 21:52:03 +0200

    Ticket factory adjsuted for new relationship (assignee_id instead of reporteer_id), User seeder seeds 2 default users & couple of dummy users

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -15,9 +16,23 @@ class UserSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'botUser',
             'email' => 'bot@user.com',
-            'password' => 'botBotBotBot',
+            'password' => Hash::make('botBotBotBot'),
             'isAdmin' => 1,
             'role' => 'pm',
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
         ]);
+
+        DB::table('users')->insert([
+            'name' => 'Alistair',
+            'email' => "marek@jankovic.site",
+            'password' => Hash::make('rastamanuf2hx'),
+            'isAdmin' => 1,
+            'role' => 'pm',
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+        ]);
+
+        factory(App\User::class, 5)->create();
     }
 }
