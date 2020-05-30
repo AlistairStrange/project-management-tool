82323cf2ff90f07f6417d8ff0026349188871640
Marek <marekjan23@gmail.com>
2020-05-30 11:49:58 +0200

    Fresh & empty ProjectBoard CRUD controller

---
+++ app/Http/Controllers/ProjectBoardController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ProjectBoard;
+use Illuminate\Http\Request;
+
+class ProjectBoardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ProjectBoard  $projectBoard
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProjectBoard $projectBoard)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ProjectBoard  $projectBoard
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProjectBoard $projectBoard)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ProjectBoard  $projectBoard
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProjectBoard $projectBoard)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ProjectBoard  $projectBoard
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProjectBoard $projectBoard)
+    {
+        //
+    }
+}
