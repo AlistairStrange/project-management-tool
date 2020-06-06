c9d8f1b7a3216d47d4da017a8246ca8cb1dd8d9a
Marek <marekjan23@gmail.com>
2020-06-06 12:39:01 +0200

    global navigation - added project board link & adjusted Create ticket btn margins

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,19 +39,17 @@
             </div>
             <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                 <div class="text-sm lg:flex-grow">
-                <a href="{{ route('tickets') }}" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                    Tickets Overview
-                </a>
-                <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                    Examples
-                </a>
-                <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-                    Blog
-                </a>
+                    <a href="{{ route('tickets') }}" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                        Tickets
+                    </a>
+
+                    <a href="{{ route('project.index') }}" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                        Project Boards
+                    </a>
                 </div>
                     
                 <div class="flex-1 text-right">
-                    <a href="{{ route('create-ticket') }}" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white
+                    <a href="{{ route('create-ticket') }}" class="inline-block text-sm px-4 py-2 mr-4 leading-none border rounded text-white border-white
                      hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Create a ticket</a>
                     @guest
                         <a class="no-underline hover:underline text-gray-300 text-sm p-3" href="{{ route('login') }}">{{ __('Login') }}</a>
