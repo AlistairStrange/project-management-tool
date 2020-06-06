f844dd68d5c9319662be162883dcd3697256719f
Marek <marekjan23@gmail.com>
2020-06-06 11:27:39 +0200

    passing new variable to form component - projects

--- resources/views/tickets/edit.blade.php
+++ resources/views/tickets/edit.blade.php
@@ -10,7 +10,7 @@
         <div class="container mx-auto">
                 <div class="max-w-lg rounded overflow-hidden shadow-lg mx-auto">
                     <div class="px-6 py-4">
-                        <x-ticket-form :ticket="$ticket"/>
+                        <x-ticket-form :ticket="$ticket" :projects="$projects"/>
                     </div>
                 </div>
         </div>
