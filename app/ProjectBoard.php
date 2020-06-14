3485b4795e188baac69a3105b588d7d596e3026c
Marek <marekjan23@gmail.com>
2020-06-14 16:12:38 +0200

    Soft deletes added to Project Boards

--- app/ProjectBoard.php
+++ app/ProjectBoard.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProjectBoard extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'name',
         'description',
