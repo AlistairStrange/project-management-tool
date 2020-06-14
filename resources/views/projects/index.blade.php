1520de063eb6144ad2521a51238e2467e498aa7b
Marek <marekjan23@gmail.com>
2020-06-14 16:13:32 +0200

    Links added on abbreviation & name of the project, button for creating new projects

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -7,16 +7,26 @@
                 <tr class="divide-x divide-gray-400">
                     <th class="px-4 py-2">ID</th>
                     <th class="px-4 py-2">Name</th>
-                    <th class="px-4 py-2"># of tickets</th>
+                    <th class="pl-4 py-2">Tickets</th>
                 </tr>
             </thead>
             <tbody>
                 @if(isset($projects))
                     @foreach($projects as $project)
                         <tr class="divide-x divide-gray-400">
-                            <td class="px-4 py-2">{{ $project->abbreviation }}</td>
-                            <td class="px-4 py-2">{{ $project->name }}</td>
-                            <td class="px-4 py-2">{{ $project->tickets()->get()->count() }}</td>
+                            <td class="px-4 py-2">
+                                <a href="{{route('project.show', $project->id)}}" class="hover:underline text-teal-500">
+                                    {{ $project->abbreviation }}
+                                </a>
+                            </td>
+
+                            <td class="px-4 py-2">
+                                <a href="{{route('project.show', $project->id)}}" class="hover:underline text-teal-500">
+                                    {{ $project->name }}
+                                </a>
+                            </td>
+
+                            <td class="px-4 py-2 text-center">{{ $project->tickets()->get()->count() }}</td>
                         </tr>
                     @endforeach
                 @else
@@ -26,5 +36,11 @@
                 @endif
             </tbody>
         </table>
+
+        <div class="text-center my-8">
+            <a href="{{ route('project.create') }}">
+                <button class="border border-teal-500 hover:bg-teal-500 hover:text-white text-teal-500 font-bold py-2 px-4 rounded">Start a new Project!</button>
+            </a>
+        </div>
     </div>
 @endsection
