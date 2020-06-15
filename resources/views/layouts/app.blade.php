bda67af6ffc44fb2f5780d8802066e4e232c2d65
Marek <marekjan23@gmail.com>
2020-06-15 22:44:51 +0200

    enhanced menu, dropdown for projects, adjusted routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
         <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6 mb-5">
             <div class="flex items-center flex-shrink-0 text-white mr-6">
                 <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-                <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+                <span class="font-semibold text-xl tracking-tight">Ticketify</span>
             </div>
             <div class="block lg:hidden">
                 <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
@@ -39,13 +39,28 @@
             </div>
             <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                 <div class="text-sm lg:flex-grow">
-                    <a href="{{ route('tickets') }}" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                        Tickets
-                    </a>
 
                     <a href="{{ route('project.index') }}" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
                         Project Boards
                     </a>
+
+                    <!-- Dropdown for Tickets - showing availaible project boards -->
+                    <div class="dropdown inline-block relative">
+                        <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                            Tickets
+                        </a>
+                        
+                        <ul class="dropdown-menu absolute hidden text-gray-700 pt-1">
+                            @foreach($projectList as $key => $name)
+                                <li class="">
+                                    <a class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" 
+                                    href="{{ route('tickets', $key) }}">
+                                         {{ $key . ' - ' . $name }}
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
                 </div>
                     
                 <div class="flex-1 text-right">
