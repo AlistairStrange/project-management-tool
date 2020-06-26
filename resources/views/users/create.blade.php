86266baf7d160e9a528f187a1925c3b74fc5c97e
Marek <marekjan23@gmail.com>
2020-06-26 21:39:43 +0200

    New view with form for creating users

---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('scripts')
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+@endsection
+
+@section('css')
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
+@endsection
+
+@section('content')
+    <div class="grid grid-flow-row">
+       <div class="container mx-auto">
+            <div class="max-w-lg rounded overflow-hidden shadow-lg mx-auto">
+                <div class="px-6 py-4">
+                    <!-- FORM Component placeholder -->
+                    <x-user-form :boards="$boards"/> 
+                </div>
+            </div>
+       </div>
+   </div>
+
+   <!-- SELECT 2 Dropdown -->
+   <script type="text/javascript">
+    $(document).ready(function() {
+        $('.select2-dropdown').select2({
+            width: '100%',
+        });
+    });
+   </script>
+@endsection
