034bb757b031bf8abd0a59519b63f14a739141ac
Marek <marekjan23@gmail.com>
2020-08-29 13:36:40 +0200

    Story points added to results + styling adjustments

--- resources/views/partials/_report-tickets.blade.php
+++ resources/views/partials/_report-tickets.blade.php
@@ -1,8 +1,9 @@
-<table id="filter-results" class="mt-8 mx-auto table-auto divide-y divide-gray-300">
+<table id="filter-results" class="mt-8 mx-auto table-auto divide-y divide-gray-300 text-gray-600">
     <thead>
         <tr class="divide-x divide-gray-300">
             <th class="px-4 py-2">Ticket #</th>
             <th class="px-4 py-2">Subject</th>
+            <th class="px-4 py-2">SP</th>
             <th class="px-4 py-2">Assignee</th>
             <th class="px-4 py-2">Status</th>
             <th class="px-4 py-2">Deadline</th>
@@ -13,15 +14,23 @@
         @foreach($data as $ticket)
             <tr class="divide-x divide-gray-300 hover:bg-gray-200">
                 <td class="px-4 py-2">
-                    {{ $ticket->projectBoard->abbreviation }}
+                    {{ $ticket->projectBoard->abbreviation }}-{{ $ticket->id }}
                 </td>
 
                 <td class="px-4 py-2">
-                    {{ $ticket->subject }}
+                    <a href="{{ route('ticket.show', $ticket->id) }}" class="underline text-teal-400 hover:text-teal-600">
+                        {{ $ticket->subject }}
+                    </a>
                 </td>
 
                 <td class="px-4 py-2">
-                    {{ $ticket->user->name }}
+                    {{ $ticket->story_points }}
+                </td>
+
+                <td class="px-4 py-2">
+                    <a href="{{ route('user.show', $ticket->user->id) }}" class="underline text-teal-400 hover:text-teal-600">
+                        {{ $ticket->user->name }}
+                    </a>
                 </td>
 
                 <td class="px-4 py-2">
